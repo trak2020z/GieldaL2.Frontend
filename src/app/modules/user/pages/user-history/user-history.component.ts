9b2ae8321e03b3b305ca3c1e644da7a77e91840d
Milosz Rylski <rylec1995@gmail.com>
2019-11-27 18:48:48 +0100

    Modify user-history component to: display stock name, color code transaction type(sold, bought)

--- src/app/modules/user/pages/user-history/user-history.component.ts
+++ src/app/modules/user/pages/user-history/user-history.component.ts
@@ -2,7 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Transaction } from 'src/app/_models/transaction';
 import { TransactionService } from 'src/app/_services/transaction.service';
 import { UserHistoryDataElement } from './userHistoryDataElement';
+import { StockService } from 'src/app/_services/stock.service';
+import { forkJoin } from 'rxjs';
+import { Stock } from 'src/app/_models/stock.model';
 
+/**
+ * The User transaction history component
+ */
 @Component({
   selector: 'app-user-history',
   templateUrl: './user-history.component.html',
@@ -10,33 +16,64 @@ import { UserHistoryDataElement } from './userHistoryDataElement';
 })
 
 export class UserHistoryComponent implements OnInit {
+  /**
+   * Temporary used for test only
+   */
   readonly USER_ID: Number = 1;
 
+  /**
+   * Stores transactions history fo user
+   */
   transactions: Transaction[];
+  /**
+* Displayed column by mat-table  
+*/
   displayedColumns: string[] = ['stockName', 'amount', 'price', 'finalPrice'];
+  /**
+   * Stores data displayed by table
+   */
   dataSource: UserHistoryDataElement[] = [];
 
-  constructor(private tansactionService: TransactionService) {
+  /**
+  * Default constructor defining services
+  * 
+  * @param tansactionService
+  * @param stockService 
+  */
+  constructor(private tansactionService: TransactionService,
+    private stockService: StockService) {
   }
+
 
   ngOnInit() {
-    this.getTransactions();
+    this.getTableData();
   }
 
-  getTransactions(): void {
-    this.tansactionService.getTransactions()
-      .subscribe(t => {
+  /**
+   * Gets data from services, and filter treasactions for currently logged user
+   */
+  getTableData(): void {
+    forkJoin([
+      this.tansactionService.getTransactions(),
+      this.stockService.getStocks()
+    ]).subscribe(([t, s]: [Transaction[], Stock[]]) => {
       this.transactions = t.filter(t => t.buyerId == this.USER_ID || t.sellerId == this.USER_ID)
-        this.transactions.forEach((t: Transaction) => this.pushDataElemet(t))
-      })
+      this.transactions.forEach((t: Transaction) => this.pushDataElemet(t, s))
+    })
   }
 
-  pushDataElemet(t: Transaction): void {
+  /**
+   * Map transaction to match table row, replace stockId with stock name, and set transaction type based on selerId an buyerId
+   * @param t transaction to map
+   * @param s stock list
+   */
+  pushDataElemet(t: Transaction, s: Stock[]): void {
     var dataElement: UserHistoryDataElement = {
-      stockName: t.id.toString(),
+      stockName: s[t.stockId.toString()].name,
       amount: t.amount,
       price: t.price,
-      finalPrice: t.amount.valueOf() * t.price.valueOf()
+      finalPrice: t.amount.valueOf() * t.price.valueOf(),
+      type: (t.buyerId == this.USER_ID) ? "bougth" : (t.sellerId == this.USER_ID) ? "sold" : " ",
     }
     this.dataSource.push(dataElement);
   }
